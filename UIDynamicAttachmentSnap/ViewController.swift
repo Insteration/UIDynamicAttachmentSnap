2259bd94ac7fdff9453d4850e4d0ba8cb92427f9
Art Karma <inste@icloud.com>
2019-05-06 20:13:35 +0300

    Initial Commit

---
+++ UIDynamicAttachmentSnap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIDynamicAttachmentSnap
+//
+//  Created by Art Karma on 5/6/19.
+//  Copyright © 2019 Art Karma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
